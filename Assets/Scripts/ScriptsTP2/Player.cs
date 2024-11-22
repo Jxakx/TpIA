6a3934b9df78939a7d151b08200a62f227c16f99
Augus <145805991+au6us@users.noreply.github.com>
2024-11-21 21:15:21 -0300

    Escenario

--- Assets/Scripts/ScriptsTP2/Player.cs
+++ Assets/Scripts/ScriptsTP2/Player.cs
@@ -6,15 +6,24 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 5f; 
 
+    private Vector3 currentVelocity;
+
     private void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal"); 
-        float verticalInput = Input.GetAxis("Vertical");
+    {        
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); 
+        float verticalInput = Input.GetAxisRaw("Vertical");  
+       
+        Vector3 targetDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
         
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        if (targetDirection.magnitude > 0)
+        {
+            currentVelocity = Vector3.MoveTowards(currentVelocity, targetDirection * speed, 100 * Time.deltaTime);
+        }
+        else
+        {
+            currentVelocity = Vector3.MoveTowards(currentVelocity, Vector3.zero, 1000 * Time.deltaTime);
+        }
         
-        movement = movement.normalized * speed * Time.deltaTime;
-        
-        transform.Translate(movement, Space.World);
+        transform.Translate(currentVelocity * Time.deltaTime, Space.World);
     }
 }
